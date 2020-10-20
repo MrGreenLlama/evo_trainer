8407102e5e79370382197a7457383447eeee70c5
Jona Saffer <jona.saffer@evocortex.com>
2020-10-20 19:16:18 +0200

    fix on_chroot typo

--- pi-gen-files/custom_stage/01-ros-prepare/00-run.sh
+++ pi-gen-files/custom_stage/01-ros-prepare/00-run.sh
@@ -2,8 +2,8 @@
 
 install -Dm 644 "./files/ros.list" "${ROOTFS_DIR}/etc/apt/sources.list.d/ros-latest.list"
 
-on-chroot apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-on-chroot <<EOF
+on_chroot apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+on_chroot <<EOF
 apt-get update
 apt-get upgrade -y
 EOF
