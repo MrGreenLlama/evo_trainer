2efb82afe80e7d2060359df3e138a7ce94768d31
Jona Saffer <jona.saffer@evocortex.com>
2020-10-21 11:28:45 +0200

    avoid sudo usage

--- pi-gen-files/custom_stage/01-ros-base/01-run-chroot.sh
+++ pi-gen-files/custom_stage/01-ros-base/01-run-chroot.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -e
 
 rosdep init
+rosdep --as-root pip:false update 
 
-sudo -iu ${FIRST_USER_NAME} <<EOF
-rosdep update
+su - ${FIRST_USER_NAME} <<EOF
 mkdir -p ~/ros_catkin_ws
 cd ~/ros_catkin_ws
 rosinstall_generator ros_comm --rosdistro melodic --deps --wet-only --tar > melodic-ros_comm-wet.rosinstall
 wstool init src melodic-ros_comm-wet.rosinstall
-rosdep install -y --from-paths src --ignore-src --rosdistro melodic -r --os=debian:buster
 EOF
 
-cd ~${FIRST_USER_NAME}/ros_catkin_ws
+cd /home/${FIRST_USER_NAME}/ros_catkin_ws
+rosdep --as-root pip:false install -y --from-paths src --ignore-src --rosdistro melodic -r --os=debian:buster
 ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/melodic -j
 
-echo "source /opt/ros/melodic/setup.bash" >> ~${FIRST_USER_NAME}/.bashrc
+echo "source /opt/ros/melodic/setup.bash" >> /home/${FIRST_USER_NAME}/.bashrc
