227c9b42e08af4a0d9862f1696e11ebae0adf9bd
Jona Saffer <jona.saffer@evocortex.com>
2020-10-26 12:37:26 +0100

    force target arch

--- pi-gen-files/custom_stage/01-ros-base/01-run-chroot.sh
+++ pi-gen-files/custom_stage/01-ros-base/01-run-chroot.sh
@@ -6,12 +6,27 @@ rosdep --as-root pip:false update
 su - ${FIRST_USER_NAME} <<EOF
 mkdir -p ~/ros_catkin_ws
 cd ~/ros_catkin_ws
-rosinstall_generator ros_comm --rosdistro melodic --deps --wet-only --tar > melodic-ros_comm-wet.rosinstall
+rosinstall_generator ros_comm \
+	--rosdistro melodic \
+	--deps \
+	--wet-only \
+	--tar \
+	> melodic-ros_comm-wet.rosinstall
 wstool init src melodic-ros_comm-wet.rosinstall
 EOF
 
 cd /home/${FIRST_USER_NAME}/ros_catkin_ws
-rosdep --as-root pip:false install -y --from-paths src --ignore-src --rosdistro melodic -r --os=debian:buster
-./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/melodic -j
+rosdep --as-root pip:false install -y \
+	--from-paths src \
+	--ignore-src \
+	--rosdistro melodic \
+	-r --os=debian:buster
+./src/catkin/bin/catkin_make_isolated \
+	-DCMAKE_C_FLAGS="-march=armv6 -mfpu=vfp -mfloat-abi=hard" \
+	-DCMAKE_CXX_FLAGS="-march=armv6 -mfpu=vfp -mfloat-abi=hard" \
+	-DCMAKE_BUILD_TYPE=Release \
+	--install \
+	--install-space /opt/ros/melodic \
+	-j
 
 echo "source /opt/ros/melodic/setup.bash" >> /home/${FIRST_USER_NAME}/.bashrc
