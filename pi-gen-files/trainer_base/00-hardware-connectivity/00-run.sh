15c2adae3f0fda3946a1820b3e4a0519720fa607
Jona Saffer <jona.saffer@evocortex.com>
2020-12-07 11:34:37 +0100

    enable installed services

--- pi-gen-files/trainer_base/00-hardware-connectivity/00-run.sh
+++ pi-gen-files/trainer_base/00-hardware-connectivity/00-run.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -e
 
+# enable usb-network module
 echo -n " modules-load=dwc2,g_ether" >>"${ROOTFS_DIR}/boot/cmdline.txt"
 
-install -Dm 644 "./files/01-update-ssid.conf" "${ROOTFS_DIR}/etc/systemd/system/hostapd.service.d/01-update-ssid.conf"
-install -Dm 644 "./files/alive-pin.service" "${ROOTFS_DIR}/etc/systemd/system/alive-pin.service"
+# configure services
+install -Dm 644 "./files/01-update-ssid.conf" \
+	"${ROOTFS_DIR}/etc/systemd/system/hostapd.service.d/01-update-ssid.conf"
+install -Dm 644 "./files/alive-pin.service" \
+	"${ROOTFS_DIR}/etc/systemd/system/alive-pin.service"
 install -Dm 644 "./files/hostapd.conf" "${ROOTFS_DIR}/etc/hostapd/hostapd.conf"
 install -Dm 644 "./files/dnsmasq.conf" "${ROOTFS_DIR}/etc/dnsmasq.conf"
 cat "./files/interfaces" >>"${ROOTFS_DIR}/etc/network/interfaces"
+
+# enable services
+ln -sf "/etc/systemd/system/alive-pin.service" \
+	"${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/alive-pin.service"
+ln -sf "/usr/lib/systemd/system/hostapd.service" \
+	"${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/hostapd.service"
+ln -sf "/usr/lib/systemd/system/dnsmasq.service" \
+	"${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/dnsmasq.service"
