c03d9ec84fd14f7e1a387ecb20e6ef3ae4d41d9e
Jona Saffer <jona.saffer@evocortex.com>
2020-12-07 13:17:18 +0100

    fix ros package name reference

--- pi-gen-files/evocortex_ros/02-utility-scripts/files/run-evotrainer-controller.sh
+++ pi-gen-files/evocortex_ros/02-utility-scripts/files/run-evotrainer-controller.sh
@@ -2,5 +2,5 @@
 
 source ~/evo_catkin_ws/devel/setup.bash
 
-roslaunch evo_rd_platform_examples evo_evotrainer_base_driver.launch
+roslaunch evo_rd_platform_example evo_evotrainer_base_driver.launch
 
