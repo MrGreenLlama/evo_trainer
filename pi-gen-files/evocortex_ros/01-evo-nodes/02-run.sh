1fb7af33ddbeac9085c239634e3b164a6c44505e
Jona Saffer <jona.saffer@evocortex.com>
2020-12-08 16:58:32 +0100

    fix helperscripts

--- pi-gen-files/evocortex_ros/01-evo-nodes/02-run.sh
+++ pi-gen-files/evocortex_ros/01-evo-nodes/02-run.sh
@@ -2,10 +2,10 @@
 
 ### Install platform specific launch file
 
-EVO_CATKIN_WS=${ROOTFS}/home/${FIRST_USER_NAME}/evo_catkin_ws/
-PLATFORM_EXAMPLES=${EVO_CATKIN_WS}/src/evo_rd_platform/evo_rd_platform_example/
+EVO_CATKIN_WS=${ROOTFS}/home/${FIRST_USER_NAME}/evo_catkin_ws
+PLATFORM_EXAMPLE=${EVO_CATKIN_WS}/src/evo_rd_platform/evo_rd_platform_example
 
-install -Dm 0644 $(stat --format "-u %u -g %g" ${PLATFORM_EXAMPLES}/launch) \
-	-t ${PLATFORM_EXAMPLES}/launch/ \
-	files/evo_evotrainer_base_driver.launch
+install -Dm 0644 $(stat --format "-u %u -g %g" "${ROOTFS}/home/${FIRST_USER_NAME}") \
+	"files/evo_evotrainer_base_driver.launch" \
+	"${PLATFORM_EXAMPLE}/launch/evo_evotrainer_base_driver.launch"
 
