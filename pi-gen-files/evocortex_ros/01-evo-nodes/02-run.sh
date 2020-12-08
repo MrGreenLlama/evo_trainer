6f242e68655ad957555a3ed664cd7fba9cac7c3d
Jona Saffer <jona.saffer@evocortex.com>
2020-12-08 17:14:13 +0100

    use correct variable

--- pi-gen-files/evocortex_ros/01-evo-nodes/02-run.sh
+++ pi-gen-files/evocortex_ros/01-evo-nodes/02-run.sh
@@ -2,10 +2,10 @@
 
 ### Install platform specific launch file
 
-EVO_CATKIN_WS=${ROOTFS}/home/${FIRST_USER_NAME}/evo_catkin_ws
+EVO_CATKIN_WS=${ROOTFS_DIR}/home/${FIRST_USER_NAME}/evo_catkin_ws
 PLATFORM_EXAMPLE=${EVO_CATKIN_WS}/src/evo_rd_platform/evo_rd_platform_example
 
-install -Dm 0644 $(stat --format "-u %u -g %g" "${ROOTFS}/home/${FIRST_USER_NAME}") \
+install -Dm 0644 $(stat --format "-u %u -g %g" "${PLATFORM_EXAMPLE}/launch") \
 	"files/evo_evotrainer_base_driver.launch" \
 	"${PLATFORM_EXAMPLE}/launch/evo_evotrainer_base_driver.launch"
 
